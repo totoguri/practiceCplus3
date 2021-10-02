ded070fbcadd8cc058697452cc8763173ab5b6dd
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-02 17:03:40 +0900

    cplus3

--- Cplusplus/Cplusplus.cpp
+++ Cplusplus/Cplusplus.cpp
@@ -1,21 +1,101 @@
-// Cplusplus.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using std::cout;
+using std::endl;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//class SoBase {
+//private:
+//	int baseNum;
+//
+//public:
+//	SoBase() : baseNum(20) {
+//		cout << "SoBase()" << endl;
+//	}
+//	SoBase(int n) : baseNum(n) {
+//		cout << "SoBase(int n)" << endl;
+//	}
+//	void ShowBaseData() {
+//		cout << baseNum << endl;
+//	}
+//};
+//
+//class SoDerived : public SoBase {
+//private:
+//	int derivNum;
+//
+//public:
+//	SoDerived() : derivNum(30) {
+//		cout << "SoDerived()" << endl;
+//	}
+//	SoDerived(int n) : derivNum(n) {
+//		cout << "SoDerived(int n)" << endl;
+//	}
+//	SoDerived(int n1, int n2) : SoBase(n1), derivNum(n2) {
+//		cout << "SoDerived(int n1, int n2)" << endl;
+//	}
+//	void ShowDerivData() {
+//		ShowBaseData();
+//		cout << derivNum << endl;
+//	}
+//};
+//
+//int main() {
+//	cout << "case1..... " << endl;
+//	SoDerived dr1;
+//	dr1.ShowDerivData();
+//	cout << "-----------------------" << endl;
+//	cout << "case2..... " << endl;
+//	SoDerived dr2(12);
+//	dr2.ShowDerivData();
+//	cout << "-----------------------" << endl;
+//	cout << "case3..... " << endl;
+//	SoDerived dr3(23, 24);
+//	dr3.ShowDerivData();
+//	return 0;
+//}
+
+
+#include <cstring>
+class Person {
+private:
+	char* name;
+
+public:
+	Person(const char* myname) {
+		name = new char[strlen(myname) + 1];
+		strcpy_s(name, strlen(myname)+1, myname);
+	}
+	~Person() {
+		delete[]name;
+	}
+	void WhatYourName() const {
+		cout << "My name is " << name << endl;
+	}
+};
+
+class UnivStudent : public Person {
+private: 
+	char* major;
+
+public:
+	UnivStudent(const char* myname, const char* mymajor) : Person(myname) {
+		major = new char[strlen(mymajor) + 1];
+		strcpy_s(major, strlen(mymajor)+1, mymajor);
+	}
+	~UnivStudent() {
+		delete[]major;
+	}
+	void WhoAreYou() const {
+		WhatYourName();
+		cout << "My major is " << major << endl << endl;
+	}
+};
+
+int main() {
+	UnivStudent st1("Kim", "Mathematics");
+	st1.WhoAreYou();
+	UnivStudent st2("Hong", "Physics");
+	st2.WhoAreYou();
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-//
+// p.304
