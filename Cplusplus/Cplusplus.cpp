c78f7fc20c5e38cb52e234654beab2674b568ff8
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-07 21:39:39 +0900

    cplus

--- Cplusplus/Cplusplus.cpp
+++ Cplusplus/Cplusplus.cpp
@@ -381,35 +381,334 @@ using std::endl;
 //}
 
 
-class First {
+//class First {
+//public:
+//	virtual void MyFunc() {
+//		cout << "FirstFunc" << endl;
+//	}
+//};
+//
+//class Second : public First {
+//public:
+//	virtual void MyFunc() {
+//		cout << "SecondFunc" << endl;
+//	}
+//};
+//
+//class Third : public Second {
+//public:
+//	virtual void MyFunc() {
+//		cout << "ThirdFunc" << endl;
+//	}
+//};
+//
+//int main() {
+//	Third* tptr = new Third();
+//	Second* sptr = tptr;
+//	First* fptr = sptr;
+//
+//	fptr->MyFunc();
+//	sptr->MyFunc();
+//	tptr->MyFunc();
+//	delete tptr;
+//	return 0;
+//}
+
+
+//#include <cstring>
+//class Employee {
+//private:
+//	char name[100];
+//public:
+//	Employee(const char* name) {
+//		int len = strlen(name) + 1;
+//		strcpy_s(this->name, len, name);
+//	}
+//	void ShowYourName() const {
+//		cout << "name: " << name << endl;
+//	}
+//	virtual int GetPay() const {
+//		return 0;
+//	}
+//	virtual void ShowSalaryInfo() const {
+//		//
+//	}
+//};
+//
+//class PermanentWorker : public Employee {
+//private:
+//	int salary;
+//public:
+//	PermanentWorker(const char* name, int money) : Employee(name), salary(money) {
+//		//
+//	}
+//	int GetPay() const {
+//		return salary;
+//	}
+//	void ShowSalaryInfo() const {
+//		ShowYourName();
+//		cout << "Salary: " << GetPay() << endl << endl;
+//	}
+//};
+//
+//class TemporaryWorker : public Employee {
+//private:
+//	int workTime;
+//	int payPerHour;
+//public:
+//	TemporaryWorker(const char* name, int pay) : Employee(name), workTime(0), payPerHour(pay) {
+//		//
+//	}
+//	void AddWorkTime(int time) {
+//		workTime += time;
+//	}
+//	int GetPay() const {
+//		return workTime * payPerHour;
+//	}
+//	void ShowSalaryInfo() const {
+//		ShowYourName();
+//		cout << "salary: " << GetPay() << endl << endl;
+//	}
+//};
+//
+//class SalesWorker : public PermanentWorker {
+//private:
+//	int salesResult;
+//	double bonusRatio;
+//public:
+//	SalesWorker(const char* name, int money, double ratio) : PermanentWorker(name, money), salesResult(0), bonusRatio(ratio) {
+//		//
+//	}
+//	void AddSalesResult(int value) {
+//		salesResult += value;
+//	}
+//	int GetPay() const {
+//		return PermanentWorker::GetPay() + (int)(salesResult * bonusRatio);
+//	}
+//	void ShowSalaryInfo() const {
+//		ShowYourName();
+//		cout << "salary: " << GetPay() << endl << endl;
+//	}
+//};
+//
+//class EmployeeHandler {
+//private:
+//	Employee* empList[50];
+//	int empNum;
+//public:
+//	EmployeeHandler() : empNum(0) {
+//		//
+//	}
+//	void AddEmployee(Employee* emp) {
+//		empList[empNum++] = emp;
+//	}
+//	void ShowAllSalaryInfo() const {
+//		for (int i = 0; i < empNum; i++)
+//			empList[i]->ShowSalaryInfo();
+//	}
+//	void ShowTotalSalary() const {
+//		int sum = 0;
+//		for (int i = 0; i < empNum; i++)
+//			sum += empList[i]->GetPay();
+//		cout << "salary sum: " << sum << endl;
+//	}
+//	~EmployeeHandler() {
+//		for (int i = 0; i < empNum; i++)
+//			delete empList[i];
+//	}
+//};
+//
+//int main(void) {
+//	// 직원관리를 목적으로 설계된 컨트롤 클래스의 객체 생성
+//	EmployeeHandler handler;
+//
+//	// 정규직 등록
+//	handler.AddEmployee(new PermanentWorker("KIM", 1000));
+//	handler.AddEmployee(new PermanentWorker("LEE", 1500));
+//	
+//	// 임시직 등록
+//	TemporaryWorker* alba = new TemporaryWorker("Jung", 700);
+//	alba->AddWorkTime(5);
+//	handler.AddEmployee(alba);
+//
+//	// 영업직 등록
+//	SalesWorker* seller = new SalesWorker("Hong", 1000, 0.1);
+//	seller->AddSalesResult(7000);
+//	handler.AddEmployee(seller);
+//
+//	// 이번 달에 지불해야 할 급여의 정보
+//	handler.ShowAllSalaryInfo();
+//
+//	// 이번 달에 지불해야 할 급여의 종합
+//	handler.ShowTotalSalary();
+//	return 0;
+//}
+
+
+//class First {
+//private:
+//	char* strOne;
+//public:
+//	First(const char* str) {
+//		strOne = new char[strlen(str) + 1];
+//	}
+//	virtual ~First() {
+//		cout << "~First()" << endl;
+//		delete[]strOne;
+//	}
+//};
+//
+//class Second : public First {
+//private:
+//	char* strTwo;
+//public:
+//	Second(const char* str1, const char* str2) : First(str1) {
+//		strTwo = new char[strlen(str2) + 1];
+//	}
+//	virtual ~Second() {
+//		cout << "~Second()" << endl;
+//		delete[]strTwo;
+//	}
+//};
+//
+//int main() {
+//	First* ptr = new Second("simple", "complex");
+//	delete ptr;
+//	return 0;
+//}
+
+
+//class First {
+//public:
+//	void FirstFunc() {
+//		cout << "FirstFunc()" << endl;
+//	}
+//	virtual void SimpleFunc() {
+//		cout << "First's SimpleFunc()" << endl;
+//	}
+//};
+//
+//class Second : public First {
+//public:
+//	void SecondFunc() {
+//		cout << "SecondFunc()" << endl;
+//	}
+//	virtual void SimpleFunc() {
+//		cout << "Second's SimpleFunc()" << endl;
+//	}
+//};
+//
+//class Third : public Second {
+//public:
+//	void ThirdFunc() {
+//		cout << "ThirdFunc()" << endl;
+//	}
+//	virtual void SimpleFunc() {
+//		cout << "Third's SimpleFunc()" << endl;
+//	}
+//};
+//
+//int main() {
+//	Third obj;
+//	obj.FirstFunc();
+//	obj.SecondFunc();
+//	obj.ThirdFunc();
+//	obj.SimpleFunc();
+//
+//	Second& sref = obj;
+//	sref.FirstFunc();
+//	sref.SecondFunc();
+//	sref.SimpleFunc();
+//
+//	First& fref = obj;
+//	fref.FirstFunc();
+//	fref.SimpleFunc();
+//	return 0;
+//}
+
+
+//class Data {
+//private:
+//	int data;
+//public:
+//	Data(int num) : data(num) {
+//		//
+//	}
+//	void ShowData() {
+//		cout << "Data: " << data << endl;
+//	}
+//	void Add(int num) {
+//		data += num;
+//	}
+//};
+//
+//int main() {
+//	Data obj(15);
+//	obj.Add(17);
+//	obj.ShowData();
+//	return 0;
+//}
+
+
+//typedef struct Data {
+//	int data;
+//	void (*ShowData)(Data*);
+//	void (*Add)(Data*, int);
+//} Data;
+//
+//void ShowData(Data* THIS) {
+//	cout << "Data: " << THIS->data << endl;
+//}
+//
+//void Add(Data* THIS, int num) {
+//	THIS->data += num;
+//}
+//
+//int main() {
+//	Data obj1 = { 15, ShowData, Add };
+//	Data obj2 = { 7, ShowData, Add };
+//
+//	obj1.Add(&obj1, 17);
+//	obj2.Add(&obj2, 9);
+//	obj1.ShowData(&obj1);
+//	obj2.ShowData(&obj2);
+//	return 0;
+//}
+
+
+class AAA {
+private:
+	int num1;
 public:
-	virtual void MyFunc() {
-		cout << "FirstFunc" << endl;
+	AAA() : num1(0) {};
+	virtual void Func1() {
+		cout << "Func1" << endl;
+	}
+	virtual void Func2() {
+		cout << "Func2" << endl;
 	}
 };
 
-class Second : public First {
+class BBB : public AAA {
+private:
+	int num2;
 public:
-	virtual void MyFunc() {
-		cout << "SecondFunc" << endl;
+	BBB() : AAA(), num2(0) {};
+	virtual void Func1() {
+		cout << "BBB::Func1" << endl;
 	}
-};
-
-class Third : public Second {
-public:
-	virtual void MyFunc() {
-		cout << "ThirdFunc" << endl;
+	void Func3() {
+		cout << "Func3" << endl;
 	}
 };
 
 int main() {
-	Third* tptr = new Third();
-	Second* sptr = tptr;
-	First* fptr = sptr;
+	AAA* aptr = new AAA();
+	aptr->Func1();
 
-	fptr->MyFunc();
-	sptr->MyFunc();
-	tptr->MyFunc();
-	delete tptr;
+	BBB* bptr = new BBB();
+	bptr->Func1();
 	return 0;
 }
+
+// p.380
