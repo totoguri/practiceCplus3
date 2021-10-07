459502a00a664412fe261a0fe0e2cc8ded0d5cb5
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-07 17:58:37 +0900

    cplus

--- Cplusplus/Cplusplus.cpp
+++ Cplusplus/Cplusplus.cpp
@@ -101,71 +101,108 @@ using std::endl;
 // p.304
 
 
-#include <cstring>
-class Computer {
-private:
-	char owner[50];
-public:
-	Computer(const char* name) {
-		strcpy_s(owner, strlen(name) + 1, name);
-	}
-	void Calculate() {
-		cout << "요청 내용을 계산합니다." << endl;
-	}
-};
-
-class NotebookComp : public Computer {
-private:
-	int Battery;
-public:
-	NotebookComp(const char* name, int initChag) : Computer(name), Battery(initChag) {}
-	void Charging() {
-		Battery += 5;
-	}
-	void UseBattery() {
-		Battery -= 1;
-	}
-	void MovingCal() {
-		if (GetBatteryInfo() < 1) {
-			cout << "충전이 필요합니다." << endl;
-			return;
-		}
-		cout << "이동하면서 ";
-		Calculate();
-		UseBattery();
-	}
-	int GetBatteryInfo() {
-		return Battery;
-	}
-};
-
-class TabletNotebook : public NotebookComp {
-private:
-	char regstPenModel[50];
-public:
-	TabletNotebook(const char* name, int initChag, const char* pen) : NotebookComp(name, initChag) {
-		strcpy_s(regstPenModel, strlen(pen) + 1, pen);
-	}
-	void Write(const char* penInfo) {
-		if (GetBatteryInfo() < 1) {
-			cout << "충전이 필요합니다." << endl;
-			return;
-		}
-		if (strcmp(regstPenModel, penInfo) != 0) {
-			cout << "등록된 펜이 아닙니다.";
-			return;
-		}
-		cout << "필기 내용을 처리합니다." << endl;
-		UseBattery();
-	}
-};
-
-int main() {
-	NotebookComp nc("LEE", 5);
-	TabletNotebook tn("JEONG", 5, "ISE-241-242");
-	nc.MovingCal();
-	tn.Write("ISE-241-242");
-	return 0;
-}
+//#include <cstring>
+//class Computer {
+//private:
+//	char owner[50];
+//public:
+//	Computer(const char* name) {
+//		strcpy_s(owner, strlen(name) + 1, name);
+//	}
+//	void Calculate() {
+//		cout << "요청 내용을 계산합니다." << endl;
+//	}
+//};
+//
+//class NotebookComp : public Computer {
+//private:
+//	int Battery;
+//public:
+//	NotebookComp(const char* name, int initChag) : Computer(name), Battery(initChag) {}
+//	void Charging() {
+//		Battery += 5;
+//	}
+//	void UseBattery() {
+//		Battery -= 1;
+//	}
+//	void MovingCal() {
+//		if (GetBatteryInfo() < 1) {
+//			cout << "충전이 필요합니다." << endl;
+//			return;
+//		}
+//		cout << "이동하면서 ";
+//		Calculate();
+//		UseBattery();
+//	}
+//	int GetBatteryInfo() {
+//		return Battery;
+//	}
+//};
+//
+//class TabletNotebook : public NotebookComp {
+//private:
+//	char regstPenModel[50];
+//public:
+//	TabletNotebook(const char* name, int initChag, const char* pen) : NotebookComp(name, initChag) {
+//		strcpy_s(regstPenModel, strlen(pen) + 1, pen);
+//	}
+//	void Write(const char* penInfo) {
+//		if (GetBatteryInfo() < 1) {
+//			cout << "충전이 필요합니다." << endl;
+//			return;
+//		}
+//		if (strcmp(regstPenModel, penInfo) != 0) {
+//			cout << "등록된 펜이 아닙니다.";
+//			return;
+//		}
+//		cout << "필기 내용을 처리합니다." << endl;
+//		UseBattery();
+//	}
+//};
+//
+//int main() {
+//	NotebookComp nc("LEE", 5);
+//	TabletNotebook tn("JEONG", 5, "ISE-241-242");
+//	nc.MovingCal();
+//	tn.Write("ISE-241-242");
+//	return 0;
+//}
 
 // p.307
+
+
+//#include <cstring>
+//class Gun {
+//private:
+//	int bullet;
+//public:
+//	Gun(int bnum) : bullet(bnum) {
+//		//
+//	}
+//	void Shot() {
+//		cout << "BBANG!" << endl;
+//		bullet--;
+//	}
+//};
+//
+//class Police : public Gun {
+//private:
+//	int handcuffs;
+//public:
+//	Police(int bnum, int bcuff) : Gun(bnum), handcuffs(bcuff) {
+//		//
+//	}
+//	void PutHandcuff() {
+//		cout << "SNAP!" << endl;
+//		handcuffs--;
+//	}
+//};
+//
+//int main() {
+//	Police pman(5, 3);
+//	pman.Shot();
+//	pman.PutHandcuff();
+//	return 0;
+//}
+
+
