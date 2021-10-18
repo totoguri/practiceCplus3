37a325533fc8041e3955690b5f41374badcd243e
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-18 10:20:40 +0900

    cp

--- Cplusplus/newMain.cpp
+++ Cplusplus/newMain.cpp
@@ -524,44 +524,459 @@ using std::cin;
 //}
 
 
-#include <algorithm>
-#include <functional>
-#include <string>
-#include <vector>
+//#include <algorithm>
+//#include <functional>
+//#include <string>
+//#include <vector>
+//
+//template <typename Iter>
+//void print(Iter begin, Iter end) {
+//	while (begin != end) {
+//		cout << "[" << *begin << "] ";
+//		begin++;
+//	}
+//	cout << endl;
+//}
+//
+//int main() {
+//	std::vector<int> vec;
+//	vec.push_back(5);
+//	vec.push_back(3);
+//	vec.push_back(1);
+//	vec.push_back(2);
+//	vec.push_back(3);
+//	vec.push_back(4);
+//
+//	cout << "처음 vec 상태 ------" << endl;
+//	print(vec.begin(), vec.end());
+//
+//	cout << "벡터에서 홀수인 원소 ---" << endl;
+//	int num_erased = 0;
+//	vec.erase(std::remove_if(vec.begin(), vec.end(),
+//		[&num_erased](int i) {
+//			if (num_erased >= 2)
+//				return false;
+//			else if (i % 2 == 1) {
+//				num_erased++;
+//				return true;
+//			}
+//			return false;
+//		}),
+//		vec.end());
+//	print(vec.begin(), vec.end());
+//}
 
-template <typename Iter>
-void print(Iter begin, Iter end) {
-	while (begin != end) {
-		cout << "[" << *begin << "] ";
-		begin++;
+
+//#include <cctype>
+//#include <string>
+//
+//struct my_char_traits : public std::char_traits<char> {
+//	static int get_real_rank(char c) {
+//		if (isdigit(c)) {
+//			return c + 256;
+//		}
+//		return c;
+//	}
+//
+//	static bool lt(char c1, char c2) {
+//		return get_real_rank(c1) < get_real_rank(c2);
+//	}
+//
+//	static int compare(const char* s1, const char* s2, size_t n) {
+//		while (n-- != 0) {
+//			if (get_real_rank(*s1) < get_real_rank(*s2)) {
+//				return -1;
+//			}
+//			if (get_real_rank(*s1) > get_real_rank(*s2)) {
+//				return 1;
+//			}
+//			++s1;
+//			++s2;
+//		}
+//		return 0;
+//	}
+//};
+//
+//int main() {
+//	std::basic_string<char, my_char_traits> my_s1 = "la";
+//	std::basic_string<char, my_char_traits> my_s2 = "a1";
+//
+//	std::cout << "숫자의 우선순위가 더 낮은 문자열 : " << std::boolalpha << (my_s1 < my_s2) << endl;
+//	std::string s1 = "la";
+//	std::string s2 = "a1";
+//
+//	cout << "일반 문자열: " << std::boolalpha << (s1 < s2) << endl;
+//}
+
+
+//#include <string>
+//
+//int main() {
+//	std::u32string u32_str = U"이건 UTF-32 문자열 입니다";
+//	cout << u32_str.size() << endl;
+//}
+
+
+//#include <string>
+//
+//void* operator new(std::size_t count) {
+//	cout << count << " bytes 할당" << endl;
+//	return malloc(count);
+//}
+//
+//bool contains_very(const std::string& str) {
+//	return str.find("very") != std::string::npos;
+//}
+//
+//int main() {
+//	cout << std::boolalpha << contains_very("C++ string is very easy to use") << endl;
+//	cout << contains_very("C++ string is not easy to use") << endl;
+//}
+
+
+//#include <stdexcept>
+//
+//template <typename T>
+//class Vector {
+//public:
+//	Vector(size_t size) : size_(size) {
+//		data_ = new T[size_];
+//		for (int i = 0; i < size_; i++) {
+//			data_[i] = 3;
+//		}
+//	}
+//	const T& at(size_t index) const {
+//		if (index >= size_) {
+//			throw std::out_of_range("vector의 index가 범위를 초과하였습니다.");
+//		}
+//		return data_[index];
+//	}
+//	~Vector() { delete[] data_; }
+//
+//private:
+//	T* data_;
+//	size_t size_;
+//};
+//
+//int main() {
+//	Vector<int> vec(3);
+//	int index, data = 0;
+//	std::cin >> index;
+//
+//	try {
+//		data = vec.at(index);
+//	}
+//	catch (std::out_of_range& e) {
+//		cout << "예외 발생!" << e.what() << endl;
+//	}
+//	cout << "읽은 데이터: " << data << endl;
+//}
+
+
+//class A {
+//	int data_;
+//
+//public:
+//	A(int data) : data_(data) { cout << "일반 생성자 호출" << endl; }
+//	A(const A& a) : data_(a.data_) {
+//		cout << "복사 생성자 호출" << endl;
+//	}
+//};
+//
+//int main() {
+//	A a(1);
+//	A b(a);
+//
+//	A c(A(2));
+//}
+
+
+//#include <memory>
+//
+//class A {
+//private:
+//	int* data;
+//public:
+//	A() {
+//		cout << "자원을 획득함!" << endl;
+//		data = new int[100];
+//	}
+//	void some() {
+//		cout << "일반 포인터와 동일하게 사용 가능!" << endl;
+//	}
+//	void do_sth(int a) {
+//		std::cout << "무언가를 한다" << std::endl;
+//		data[0] = a;
+//	}
+//	~A() {
+//		cout << "자원을 해제함!" << endl;
+//		delete[] data;
+//	}
+//};
+//
+//void do_something(A* ptr) {
+//	ptr->do_sth(3);
+//}
+//
+//int main() {
+//	std::unique_ptr<A> pa(new A());
+//	do_something(pa.get());
+//}
+
+
+//#include <memory>
+//class Foo {
+//private:
+//	int a, b;
+//public:
+//	Foo(int a, int b) : a(a), b(b) {
+//		std::cout << "생성자 호출" << std::endl;
+//	}
+//	void print() {
+//		std::cout << "a : " << a << ", b : " << b << std::endl;
+//	}
+//	~Foo() {
+//		std::cout << "소멸자 호출" << std::endl;
+//	}
+//};
+//
+//int main() {
+//	auto ptr = std::make_unique<Foo>(3, 5);
+//	ptr->print();
+//}
+
+
+//#include <memory>
+//#include <vector>
+//
+//class A {
+//private:
+//	int* data;
+//public:
+//	A() {
+//		data = new int[100];
+//		std::cout << "자원을 획득함" << std::endl;
+//	}
+//	~A() {
+//		std::cout << "소멸자 호출" << std::endl;
+//		delete[] data;
+//	}
+//};
+//
+//int main() {
+//	std::vector<std::shared_ptr<A>> vec;
+//	vec.push_back(std::shared_ptr<A>(new A()));
+//	vec.push_back(std::shared_ptr<A>(vec[0]));
+//	vec.push_back(std::shared_ptr<A>(vec[1]));
+//
+//	std::cout << "첫 번째 소멸" << std::endl;
+//	vec.erase(vec.begin());
+//
+//	std::cout << "다음 원소 소멸" << std::endl;
+//	vec.erase(vec.begin());
+//
+//	std::cout << "마지막 원소 소멸" << std::endl;
+//	vec.erase(vec.begin());
+//
+//	std::cout << "프로그램 종료" << std::endl;
+//}
+
+
+//#include <memory>
+//class A : public std::enable_shared_from_this<A> {
+//	int* data;
+//public:
+//	A() {
+//		data = new int[100];
+//		std::cout << "자원을 획득함" << std::endl;
+//	}
+//	
+//	~A() {
+//		std::cout << "소멸자 호출" << std::endl;
+//		delete[] data;
+//	}
+//
+//	std::shared_ptr<A> get_shared_ptr() {
+//		return shared_from_this();
+//	}
+//};
+//
+//int main() {
+//	std::shared_ptr<A> pa1 = std::make_shared<A>();
+//	std::shared_ptr<A> pa2 = pa1->get_shared_ptr();
+//
+//	std::cout << pa1.use_count() << std::endl;
+//	std::cout << pa2.use_count() << std::endl;
+//}
+
+
+//#include <thread>
+//using std::thread;
+//
+//void func1() {
+//	for (int i = 0; i < 10; i++) {
+//		std::cout << "thread 1 working \n";
+//	}
+//}
+//
+//void func2() {
+//	for (int i = 0; i < 10; i++) {
+//		std::cout << "thread 2 working \n";
+//	}
+//}
+//
+//void func3() {
+//	for (int i = 0; i < 10; i++) {
+//		std::cout << "thread 3 working \n";
+//	}
+//}
+//
+//int main() {
+//	thread t1(func1);
+//	thread t2(func2);
+//	thread t3(func3);
+//
+//	t1.detach();
+//	t2.detach();
+//	t3.detach();
+//	cout << "메인 함수 종료 \n";
+//}
+
+
+//#include <cstdio>
+//#include <thread>
+//#include <vector>
+//using std::thread;
+//using std::vector;
+//
+//void worker(vector<int>::iterator start, vector<int>::iterator end, int* result) {
+//	int sum = 0;
+//	for (auto itr = start; itr < end; ++itr) {
+//		sum += *itr;
+//	}
+//	*result = sum;
+//
+//	thread::id this_id = std::this_thread::get_id();
+//	printf("쓰레드 %x 에서 %d 부터 %d 까지 계산한 결과 : %d \n", this_id, *start, *(end - 1), sum);
+//}
+//
+//int main() {
+//	vector<int> data(10000);
+//	for (int i = 0; i < 10000; i++) {
+//		data[i] = i;
+//	}
+//
+//	vector<int> partial_sums(4);
+//	
+//	vector<thread> workers;
+//	for (int i = 0; i < 4; i++) {
+//		workers.push_back(thread(worker, data.begin() + i * 2500, data.begin() + (i + 1) * 2500, &partial_sums[i]));
+//	}
+//
+//	for (int i = 0; i < 4; i++) {
+//		workers[i].join();
+//	}
+//
+//	int total = 0;
+//	for (int i = 0; i < 4; i++) {
+//		total += partial_sums[i];
+//	}
+//	std::cout << "전체 합: " << total << endl;
+//}
+
+
+//#include <thread>
+//#include <vector>
+//void worker(int& counter) {
+//	for (int i = 0; i < 10000; i++) {
+//		counter += 1;
+//	}
+//}
+//
+//int main() {
+//	int counter = 0;
+//	std::vector<std::thread> workers;
+//	for (int i = 0; i < 4; i++) {
+//		workers.push_back(std::thread(worker, std::ref(counter)));
+//	}
+//
+//	for (int i = 0; i < 4; i++) {
+//		workers[i].join();
+//	}
+//
+//	std::cout << "Counter 최종 값: " << counter << std::endl;
+//}
+
+
+//#include <mutex>
+//#include <thread>
+//#include <vector>
+//
+//void worker(int& result, std::mutex& m) {
+//	for (int i = 0; i < 10000; i++) {
+//		std::lock_guard<std::mutex> lock(m);
+//		result += 1;
+//	}
+//}
+//
+//int main() {
+//	int counter = 0;
+//	std::mutex m;
+//
+//	std::vector<std::thread> workers;
+//	for (int i = 0; i < 4; i++) {
+//		workers.push_back(std::thread(worker, std::ref(counter), std::ref(m)));
+//	}
+//
+//	for (int i = 0; i < 4; i++) {
+//		workers[i].join();
+//	}
+//
+//	std::cout << "Counter 최종 값: " << counter << std::endl;
+//}
+
+
+#include <mutex>
+#include <thread>
+void worker1(std::mutex& m1, std::mutex& m2) {
+	for (int i = 0; i < 1000; i++) {
+		m1.lock();
+		m2.lock();
+		std::cout << "Worker1 Hi! " << i << std::endl;
+
+		m2.unlock();
+		m1.unlock();
 	}
-	cout << endl;
+}
+
+void worker2(std::mutex& m1, std::mutex& m2) {
+	for (int i = 0; i < 1000; i++) {
+		while (true) {
+			m2.lock();
+
+			if (!m1.try_lock()) {
+				m2.unlock();
+				continue;
+			}
+
+			std::cout << "Worker2 Hi! " << i << std::endl;
+			m1.unlock();
+			m2.unlock();
+			break;
+		}
+	}
 }
 
 int main() {
-	std::vector<int> vec;
-	vec.push_back(5);
-	vec.push_back(3);
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
-	vec.push_back(4);
+	std::mutex m1, m2;
 
-	cout << "처음 vec 상태 ------" << endl;
-	print(vec.begin(), vec.end());
+	std::thread t1(worker1, std::ref(m1), std::ref(m2));
+	std::thread t2(worker2, std::ref(m1), std::ref(m2));
 
-	cout << "벡터에서 홀수인 원소 ---" << endl;
-	int num_erased = 0;
-	vec.erase(std::remove_if(vec.begin(), vec.end(),
-		[&num_erased](int i) {
-			if (num_erased >= 2)
-				return false;
-			else if (i % 2 == 1) {
-				num_erased++;
-				return true;
-			}
-			return false;
-		}),
-		vec.end());
-	print(vec.begin(), vec.end());
+	t1.join();
+	t2.join();
+
+	std::cout << "END!" << std::endl;
 }
+
+// p.755
