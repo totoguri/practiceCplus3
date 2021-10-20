a7b85a01ea276d15f2430f994778345d7f191a2b
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-20 11:33:58 +0900

    cp

--- Cplusplus/newMain.cpp
+++ Cplusplus/newMain.cpp
@@ -936,47 +936,63 @@ using std::cin;
 //}
 
 
-#include <mutex>
-#include <thread>
-void worker1(std::mutex& m1, std::mutex& m2) {
-	for (int i = 0; i < 1000; i++) {
-		m1.lock();
-		m2.lock();
-		std::cout << "Worker1 Hi! " << i << std::endl;
-
-		m2.unlock();
-		m1.unlock();
-	}
-}
-
-void worker2(std::mutex& m1, std::mutex& m2) {
-	for (int i = 0; i < 1000; i++) {
-		while (true) {
-			m2.lock();
-
-			if (!m1.try_lock()) {
-				m2.unlock();
-				continue;
-			}
-
-			std::cout << "Worker2 Hi! " << i << std::endl;
-			m1.unlock();
-			m2.unlock();
-			break;
-		}
-	}
-}
-
-int main() {
-	std::mutex m1, m2;
-
-	std::thread t1(worker1, std::ref(m1), std::ref(m2));
-	std::thread t2(worker2, std::ref(m1), std::ref(m2));
-
-	t1.join();
-	t2.join();
-
-	std::cout << "END!" << std::endl;
-}
+//#include <mutex>
+//#include <thread>
+//void worker1(std::mutex& m1, std::mutex& m2) {
+//	for (int i = 0; i < 1000; i++) {
+//		m1.lock();
+//		m2.lock();
+//		std::cout << "Worker1 Hi! " << i << std::endl;
+//
+//		m2.unlock();
+//		m1.unlock();
+//	}
+//}
+//
+//void worker2(std::mutex& m1, std::mutex& m2) {
+//	for (int i = 0; i < 1000; i++) {
+//		while (true) {
+//			m2.lock();
+//
+//			if (!m1.try_lock()) {
+//				m2.unlock();
+//				continue;
+//			}
+//
+//			std::cout << "Worker2 Hi! " << i << std::endl;
+//			m1.unlock();
+//			m2.unlock();
+//			break;
+//		}
+//	}
+//}
+//
+//int main() {
+//	std::mutex m1, m2;
+//
+//	std::thread t1(worker1, std::ref(m1), std::ref(m2));
+//	std::thread t2(worker2, std::ref(m1), std::ref(m2));
+//
+//	t1.join();
+//	t2.join();
+//
+//	std::cout << "END!" << std::endl;
+//}
 
 // p.755
+
+
+//class A {
+//public:
+//	A() {
+//		std::cout << "A constructor" << std::endl;
+//	}
+//};
+//
+//int main() {
+//	A a{};
+//}
+
+// end and restart
+
+
